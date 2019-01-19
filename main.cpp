562a50746bccd96d1543299922646bc51643fd76
Messjer <hwh16@mails.tsinghua.edu.cn>
2019-01-19 10:43:33 +0800

    outputting file name

--- main.cpp
+++ main.cpp
@@ -4,10 +4,15 @@
 using namespace std;
 
 int main(int argc, char **argv) {
+    string name;
+    if (argc == 2)
+        name = argv[1];
+    else
+        name = "try.png";
     stage::Stage stg("input.txt");
     cout <<stg;
     Canvas *canvas = stg.ray_trace();
-    canvas->draw_to_file("try.png");
+    canvas->draw_to_file(name);
     delete canvas;
     return 0;
 }
